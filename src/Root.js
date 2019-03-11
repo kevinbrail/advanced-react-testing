0f82fe46536e821e5534767ab10e5c001d80efca
Kevin Brail <kevinabrail@gmail.com>
2019-03-11 14:10:44 -0400

    added json validator middleware

--- src/Root.js
+++ src/Root.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
-import reduxPromise from 'redux-promise';
+import stateValidator from 'middlewares/stateValidator'
+import async from 'middlewares/async'
 
 
 export default ({children, initialState = {} }) => {
     const store = createStore (
         reducers, 
         initialState, 
-        applyMiddleware(reduxPromise)
+        applyMiddleware(async, stateValidator)
         );
     return (
         <Provider store={store}>{children}
